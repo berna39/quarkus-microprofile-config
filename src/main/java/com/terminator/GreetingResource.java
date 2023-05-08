8543a314ebb89a06d3491c6d1f226e91a01132c7
berna39 <kalemajoseph39@gmail.com>
2023-05-08 13:11:08 +0200

    using configuration profile

--- src/main/java/com/terminator/GreetingResource.java
+++ src/main/java/com/terminator/GreetingResource.java
@@ -1,22 +1,49 @@
 package com.terminator;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
-    @Inject
-    @ConfigProperty(name = "application.message")
+    @ConfigProperty(name = "application.message", defaultValue = "Hello from Akatsuki")
     String message;
+
+    @ConfigProperty(name = "quarkus.application.name")
+    String appName;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return String.format("Message: %s", message);
+    }
+
+    @GET
+    @Path("/name")
+    public String getAppName(){
+        return appName;
+    }
+
+    @GET
+    @Path("/provider")
+    public String getFromConfigProvider(){
+        Config config = ConfigProvider.getConfig();
+        String message = String.format("message: %s", config.getValue("application.message", String.class));
+        return message;
+    }
+
+    @GET
+    @Path("/optional-provider")
+    public String getFromOptionalConfigProvider(){
+        Config config = ConfigProvider.getConfig();
+        String value = config.getOptionalValue("application.message", String.class)
+                .orElse("Optional message");
+        String message = String.format("message: %s", value);
+        return message;
     }
 }
